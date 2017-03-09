ef8ef39c7b276f173fbaa652b77fe55518b37cb3
jamie <neworderofjamie@gmail.com>
2017-03-09 15:48:29 +0000

    improved half-float functionality - provided optimised ARM version

--- lib/include/half_float.h
+++ lib/include/half_float.h
@@ -64,6 +64,29 @@
 //----------------------------------------------------------------------------
 namespace HalfFloat
 {
+#ifdef __aarch64__
+inline uint16_t __fcvt_f32_f16(float x)
+{
+    register uint16_t r;
+
+    asm volatile("fcvt %h[r], %s[x]"
+                : [r] "=w" (r) : [x] "w" (x));
+
+    return r;
+}
+
+inline float __fcvt_f16_f32(uint16_t x)
+{
+  register float r;
+
+  asm volatile("fcvt %s[r], %h[x]"
+              : [r] "=w" (r) : [x] "w" (x));
+
+  return r;
+}
+
+#endif  // __aarch64__
+
 union FP32
 {
     uint32_t u;
@@ -87,15 +110,13 @@ union FP16
     };
 };
 
-// Original ISPC reference version; this always rounds ties up.
-inline FP16 float_to_half_full(FP32 f)
+inline uint16_t float_to_half_reference(float in)
 {
-    FP16 o = { 0 };
+    FP32 f = {.f = in};
+    FP16 o = {.u = 0};
 
     // Based on ISPC reference code (with minor modifications)
-    if (f.Exponent == 0) // Signed zero/denormal (which will underflow)
-        o.Exponent = 0;
-    else if (f.Exponent == 255) // Inf or NaN (all exponent bits set)
+    if (f.Exponent == 255) // Inf or NaN (all exponent bits set)
     {
         o.Exponent = 31;
         o.Mantissa = f.Mantissa ? 0x200 : 0; // NaN->qNaN and Inf->Inf
@@ -103,14 +124,14 @@ inline FP16 float_to_half_full(FP32 f)
     else // Normalized number
     {
         // Exponent unbias the single, then bias the halfp
-        int newexp = f.Exponent - 127 + 15;
+        const int newexp = f.Exponent - 127 + 15;
         if (newexp >= 31) // Overflow, return signed infinity
             o.Exponent = 31;
         else if (newexp <= 0) // Underflow
         {
             if ((14 - newexp) <= 24) // Mantissa might be non-zero
             {
-                unsigned int mant = f.Mantissa | 0x800000; // Hidden 1 bit
+                const unsigned int mant = f.Mantissa | 0x800000; // Hidden 1 bit
                 o.Mantissa = mant >> (14 - newexp);
                 if ((mant >> (13 - newexp)) & 1) // Check for rounding
                     o.u++; // Round, might overflow into exp bit, but this is OK
@@ -126,267 +147,18 @@ inline FP16 float_to_half_full(FP32 f)
     }
 
     o.Sign = f.Sign;
-    return o;
+    return o.u;
 }
 
-// Same as above, but with full round-to-nearest-even.
-inline FP16 float_to_half_full_rtne(FP32 f)
+inline float half_to_float_reference(uint16_t in)
 {
-    FP16 o = { 0 };
-
-    // Based on ISPC reference code (with minor modifications)
-    if (f.Exponent == 0) // Signed zero/denormal (which will underflow)
-        o.Exponent = 0;
-    else if (f.Exponent == 255) // Inf or NaN (all exponent bits set)
-    {
-        o.Exponent = 31;
-        o.Mantissa = f.Mantissa ? 0x200 : 0; // NaN->qNaN and Inf->Inf
-    }
-    else // Normalized number
-    {
-        // Exponent unbias the single, then bias the halfp
-        int newexp = f.Exponent - 127 + 15;
-        if (newexp >= 31) // Overflow, return signed infinity
-            o.Exponent = 31;
-        else if (newexp <= 0) // Underflow
-        {
-            if ((14 - newexp) <= 24) // Mantissa might be non-zero
-            {
-                unsigned int mant = f.Mantissa | 0x800000; // Hidden 1 bit
-                unsigned int shift = 14 - newexp;
-                o.Mantissa = mant >> shift;
-
-                unsigned int lowmant = mant & ((1 << shift) - 1);
-                unsigned int halfway = 1 << (shift - 1);
-
-                if (lowmant >= halfway) // Check for rounding
-                {
-                    if (lowmant > halfway || (o.Mantissa & 1)) // if above halfway point or unrounded result is odd
-                        o.u++; // Round, might overflow into exp bit, but this is OK
-                }
-            }
-        }
-        else
-        {
-            o.Exponent = newexp;
-            o.Mantissa = f.Mantissa >> 13;
-            if (f.Mantissa & 0x1000) // Check for rounding
-            {
-                if (((f.Mantissa & 0x1fff) > 0x1000) || (o.Mantissa & 1)) // if above halfway point or unrounded result is odd
-                    o.u++; // Round, might overflow to inf, this is OK
-            }
-        }
-    }
-
-    o.Sign = f.Sign;
-    return o;
-}
-
-inline FP16 float_to_half_fast(FP32 f)
-{
-    FP16 o = { 0 };
-
-    // Based on ISPC reference code (with minor modifications)
-    if (f.Exponent == 255) // Inf or NaN (all exponent bits set)
-    {
-        o.Exponent = 31;
-        o.Mantissa = f.Mantissa ? 0x200 : 0; // NaN->qNaN and Inf->Inf
-    }
-    else // Normalized number
-    {
-        // Exponent unbias the single, then bias the halfp
-        int newexp = f.Exponent - 127 + 15;
-        if (newexp >= 31) // Overflow, return signed infinity
-            o.Exponent = 31;
-        else if (newexp <= 0) // Underflow
-        {
-            if ((14 - newexp) <= 24) // Mantissa might be non-zero
-            {
-                unsigned int mant = f.Mantissa | 0x800000; // Hidden 1 bit
-                o.Mantissa = mant >> (14 - newexp);
-                if ((mant >> (13 - newexp)) & 1) // Check for rounding
-                    o.u++; // Round, might overflow into exp bit, but this is OK
-            }
-        }
-        else
-        {
-            o.Exponent = newexp;
-            o.Mantissa = f.Mantissa >> 13;
-            if (f.Mantissa & 0x1000) // Check for rounding
-                o.u++; // Round, might overflow to inf, this is OK
-        }
-    }
-
-    o.Sign = f.Sign;
-    return o;
-}
-
-inline FP16 float_to_half_fast2(FP32 f)
-{
-    FP32 infty = { 31 << 23 };
-    FP32 magic = { 15 << 23 };
-    FP16 o = { 0 };
-
-    unsigned int sign = f.Sign;
-    f.Sign = 0;
-
-    // Based on ISPC reference code (with minor modifications)
-    if (f.Exponent == 255) // Inf or NaN (all exponent bits set)
-    {
-        o.Exponent = 31;
-        o.Mantissa = f.Mantissa ? 0x200 : 0; // NaN->qNaN and Inf->Inf
-    }
-    else // (De)normalized number or zero
-    {
-        f.u &= ~0xfff; // Make sure we don't get sticky bits
-        // Not necessarily the best move in terms of accuracy, but matches behavior
-        // of other versions.
-
-        // Shift exponent down, denormalize if necessary.
-        // NOTE This represents half-float denormals using single precision denormals.
-        // The main reason to do this is that there's no shift with per-lane variable
-        // shifts in SSE*, which we'd otherwise need. It has some funky side effects
-        // though:
-        // - This conversion will actually respect the FTZ (Flush To Zero) flag in
-        //   MXCSR - if it's set, no half-float denormals will be generated. I'm
-        //   honestly not sure whether this is good or bad. It's definitely interesting.
-        // - If the underlying HW doesn't support denormals (not an issue with Intel
-        //   CPUs, but might be a problem on GPUs or PS3 SPUs), you will always get
-        //   flush-to-zero behavior. This is bad, unless you're on a CPU where you don't
-        //   care.
-        // - Denormals tend to be slow. FP32 denormals are rare in practice outside of things
-        //   like recursive filters in DSP - not a typical half-float application. Whether
-        //   FP16 denormals are rare in practice, I don't know. Whatever slow path your HW
-        //   may or may not have for denormals, this may well hit it.
-        f.f *= magic.f;
-
-        f.u += 0x1000; // Rounding bias
-        if (f.u > infty.u) f.u = infty.u; // Clamp to signed infinity if overflowed
-
-        o.u = f.u >> 13; // Take the bits!
-    }
-
-    o.Sign = sign;
-    return o;
-}
-
-inline FP16 float_to_half_fast3(FP32 f)
-{
-    FP32 f32infty = { 255 << 23 };
-    FP32 f16infty = { 31 << 23 };
-    FP32 magic = { 15 << 23 };
-    unsigned int sign_mask = 0x80000000u;
-    unsigned int round_mask = ~0xfffu;
-    FP16 o = { 0 };
-
-    unsigned int sign = f.u & sign_mask;
-    f.u ^= sign;
-
-    // NOTE all the integer compares in this function can be safely
-    // compiled into signed compares since all operands are below
-    // 0x80000000. Important if you want fast straight SSE2 code
-    // (since there's no unsigned PCMPGTD).
-
-    if (f.u >= f32infty.u) // Inf or NaN (all exponent bits set)
-        o.u = (f.u > f32infty.u) ? 0x7e00 : 0x7c00; // NaN->qNaN and Inf->Inf
-    else // (De)normalized number or zero
-    {
-        f.u &= round_mask;
-        f.f *= magic.f;
-        f.u -= round_mask;
-        if (f.u > f16infty.u) f.u = f16infty.u; // Clamp to signed infinity if overflowed
-
-        o.u = f.u >> 13; // Take the bits!
-    }
-
-    o.u |= sign >> 16;
-    return o;
-}
-
-// Same, but rounding ties to nearest even instead of towards +inf
-inline FP16 float_to_half_fast3_rtne(FP32 f)
-{
-    FP32 f32infty = { 255 << 23 };
-    FP32 f16max   = { (127 + 16) << 23 };
-    FP32 denorm_magic = { ((127 - 15) + (23 - 10) + 1) << 23 };
-    unsigned int sign_mask = 0x80000000u;
-    FP16 o = { 0 };
-
-    unsigned int sign = f.u & sign_mask;
-    f.u ^= sign;
-
-    // NOTE all the integer compares in this function can be safely
-    // compiled into signed compares since all operands are below
-    // 0x80000000. Important if you want fast straight SSE2 code
-    // (since there's no unsigned PCMPGTD).
-
-    if (f.u >= f16max.u) // result is Inf or NaN (all exponent bits set)
-        o.u = (f.u > f32infty.u) ? 0x7e00 : 0x7c00; // NaN->qNaN and Inf->Inf
-    else // (De)normalized number or zero
-    {
-        if (f.u < (113 << 23)) // resulting FP16 is subnormal or zero
-        {
-            // use a magic value to align our 10 mantissa bits at the bottom of
-            // the float. as long as FP addition is round-to-nearest-even this
-            // just works.
-            f.f += denorm_magic.f;
-
-            // and one integer subtract of the bias later, we have our final float!
-            o.u = f.u - denorm_magic.u;
-        }
-        else
-        {
-            unsigned int mant_odd = (f.u >> 13) & 1; // resulting mantissa is odd
-
-            // update exponent, rounding bias part 1
-            f.u += ((15 - 127) << 23) + 0xfff;
-            // rounding bias part 2
-            f.u += mant_odd;
-            // take the bits!
-            o.u = f.u >> 13;
-        }
-    }
-
-    o.u |= sign >> 16;
-    return o;
-}
-
-// Approximate solution. This is faster but converts some sNaNs to
-// infinity and doesn't round correctly. Handle with care.
-inline FP16 approx_float_to_half(FP32 f)
-{
-    FP32 f32infty = { 255 << 23 };
-    FP32 f16max = { (127 + 16) << 23 };
-    FP32 magic = { 15 << 23 };
-    FP32 expinf = { (255 ^ 31) << 23 };
-    unsigned int sign_mask = 0x80000000u;
-    FP16 o = { 0 };
-
-    unsigned int sign = f.u & sign_mask;
-    f.u ^= sign;
-
-    if (!(f.f < f32infty.u)) // Inf or NaN
-        o.u = f.u ^ expinf.u;
-    else
-    {
-        if (f.f > f16max.f) f.f = f16max.f;
-        f.f *= magic.f;
-    }
-
-    o.u = f.u >> 13; // Take the mantissa bits
-    o.u |= sign >> 16;
-    return o;
-}
-
-// from half->float code - just for verification.
-inline FP32 half_to_float(FP16 h)
-{
-    static const FP32 magic = { 113 << 23 };
+    FP32 h = {.u = in};
+    static const FP32 magic = {.u = 113 << 23};
     static const unsigned int shifted_exp = 0x7c00 << 13; // exponent mask after shift
     FP32 o;
 
     o.u = (h.u & 0x7fff) << 13;     // exponent/mantissa bits
-    unsigned int exp = shifted_exp & o.u;   // just the exponent
+    const unsigned int exp = shifted_exp & o.u;   // just the exponent
     o.u += (127 - 15) << 23;        // exponent adjust
 
     // handle exponent special cases
@@ -399,12 +171,24 @@ inline FP32 half_to_float(FP16 h)
     }
 
     o.u |= (h.u & 0x8000) << 16;    // sign bit
-    return o;
+    return o.f;
 }
 
-inline FP32 half_to_float_lit(uint16_t u)
+inline uint16_t float_to_half(float in)
 {
-    FP16 fp16 = { u };
-    return half_to_float(fp16);
+#ifdef __aarch64__
+    return __fcvt_f32_f16(in);
+#else
+    return float_to_half_reference(in);
+#endif  
+}
+
+inline float half_to_float(uint16_t in)
+{
+#ifdef __aarch64__
+    return __fcvt_f16_f32(in);
+#else
+    return half_to_float_reference(in);
+#endif 
 }
 }
