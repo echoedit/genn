3b6a1c0584604c4e77724aaf5274b33861f84fd9
jamie <neworderofjamie@gmail.com>
2017-04-27 16:25:17 +0100

    Multithreaded simcode - reads camera input on one thread and runs GeNN on other

--- userproject/MBody_map_project/model/classol_sim.cc
+++ userproject/MBody_map_project/model/classol_sim.cc
@@ -19,13 +19,180 @@
 //--------------------------------------------------------------------------
 
 
+
+#include <chrono>
+#include <thread>
+
+#include <csignal>
+
+// OpenCV includes
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#ifndef CPU_ONLY
+#include <opencv2/gpu/gpu.hpp>
+#endif  // CPU_ONLY
+
 #include "classol_sim.h"
+
+typedef std::chrono::high_resolution_clock sim_clock;
+
+namespace
+{
+volatile std::sig_atomic_t g_SignalStatus;
+
+void signalHandler(int status)
+{
+    g_SignalStatus = status;
+}
 
 //--------------------------------------------------------------------------
 /*! \brief This function is the entry point for running the simulation of the MBody1 model network.
 */
 //--------------------------------------------------------------------------
 
+void gennThreadHandler(int which, FILE *osf, FILE *osf2, classol &locust)
+{
+    typedef std::chrono::duration<double, std::milli> double_ms;
+
+    //------------------------------------------------------------------
+    // output general parameters to output file and start the simulation
+
+    fprintf(stdout, "# We are running with fixed time step %f \n", DT);
+    t= 0.0;
+    iT= 0;
+    int done= 0;
+    float last_t_report=  t;
+    timer.startTimer();
+
+    sim_clock::duration totalSleepTime{0};
+    sim_clock::duration totalOverrunTime{0};
+
+    const double_ms dtDurationMs{DT};
+    const sim_clock::duration dtDuration = std::chrono::duration_cast<sim_clock::duration>(dtDurationMs);
+
+#ifndef CPU_ONLY
+    if (which == GPU){
+        while (g_SignalStatus == 0)
+        {
+            // Get time step started at
+            const auto stepStart = sim_clock::now();
+
+            locust.runGPU(DT); // run next batch
+            pullDNCurrentSpikesFromDevice();
+
+#ifdef TIMING
+            fprintf(timeros, "%f %f %f \n", neuron_tme, synapse_tme, learning_tme);
+#endif
+            locust.sum_spikes();
+            locust.outputDN_spikes(osf2, which);
+
+            fprintf(osf, "%f ", t);
+            fprintf(osf,"\n");
+
+            const auto stepEnd = sim_clock::now();
+            const auto stepLength = stepEnd - stepStart;
+
+            if(stepLength > dtDuration) {
+                totalOverrunTime += (stepLength - dtDuration);
+            }
+            else {
+                const auto sleepTime = dtDuration - stepLength;
+                std::this_thread::sleep_for(sleepTime);
+                totalSleepTime += sleepTime;
+            }
+        }
+    }
+#endif
+
+    if (which == CPU){
+        while (g_SignalStatus == 0)
+        {
+            // Get time step started at
+            const auto stepStart = sim_clock::now();
+
+            locust.runCPU(DT); // run next batch
+
+#ifdef TIMING
+            fprintf(timeros, "%f %f %f \n", neuron_tme, synapse_tme, learning_tme);
+#endif
+            locust.sum_spikes();
+            locust.outputDN_spikes(osf2, which);
+
+            const auto stepEnd = sim_clock::now();
+            const auto stepLength = stepEnd - stepStart;
+
+            if(stepLength > dtDuration) {
+                totalOverrunTime += (stepLength - dtDuration);
+            }
+            else {
+                const auto sleepTime = dtDuration - stepLength;
+                std::this_thread::sleep_for(sleepTime);
+                totalSleepTime += sleepTime;
+            }
+        }
+    }
+    timer.stopTimer();
+    printf("Ran for %fms, overran by %fms and slept for %fms\n", t, double_ms(totalOverrunTime).count() , double_ms(totalSleepTime).count());
+}
+
+void cameraThreadHandler(unsigned int device)
+{
+    typedef std::chrono::duration<double> double_s;
+
+    // Check camera has opened correctly
+    cv::VideoCapture camera(device);
+    if(!camera.isOpened()) {
+        throw std::runtime_error("Cannot open camera");
+    }
+
+
+    // Get frame dimensions
+    const unsigned int width = camera.get(CV_CAP_PROP_FRAME_WIDTH);
+    const unsigned int height = camera.get(CV_CAP_PROP_FRAME_HEIGHT);
+
+    const unsigned int margin = (width - height) / 2;
+    const cv::Rect square(cv::Point(margin, 0), cv::Point(width - margin, height));
+
+    // Read first frame so we can create ROI
+    cv::Mat rawFrame;
+    if(!camera.read(rawFrame)) {
+        throw std::runtime_error("Cannot read first frame");
+    }
+
+    // Create ROI
+    cv::Mat squareROI = rawFrame(square);
+    cv::Mat greyscaleFrame;
+    cv::Mat downsampledFrame;
+
+    cv::namedWindow("Frame", CV_WINDOW_NORMAL);
+    cv::resizeWindow("Frame", 320, 320);
+
+    const auto cameraBegin = sim_clock::now();
+    unsigned int i;
+    for (i = 0; g_SignalStatus == 0; i++)
+    {
+        // Read frame
+        if(!camera.read(rawFrame)) {
+            throw std::runtime_error("Cannot read frame");
+        }
+
+        // Convert frame to greyscale
+        cv::cvtColor(squareROI, greyscaleFrame, CV_BGR2GRAY);
+
+        // Resample
+        cv::resize(greyscaleFrame, downsampledFrame,
+                   cv::Size(32, 32));
+
+        cv::imshow("Frame", downsampledFrame);
+        cv::waitKey(1);
+    }
+
+    const auto cameraEnd = sim_clock::now();
+    const auto cameraTimeS = double_s(cameraEnd - cameraBegin);
+    printf("%f FPS\n", (double)(i - 1) / cameraTimeS.count());
+}
+} // Anonymous namespace
 
 int main(int argc, char *argv[])
 {
@@ -137,70 +304,14 @@ int main(int argc, char *argv[])
 
     fprintf(stdout, "# neuronal circuitery built, start computation ... \n\n");
 
-    //------------------------------------------------------------------
-    // output general parameters to output file and start the simulation
+    std::signal(SIGINT, signalHandler);
 
-    fprintf(stdout, "# We are running with fixed time step %f \n", DT);
-    t= 0.0;
-    iT= 0;
-    int done= 0;
-    float last_t_report=  t;
-    timer.startTimer();
+    // Start threads
+    std::thread gennThread(gennThreadHandler, which, osf, osf2, std::ref(locust));
+    std::thread cameraThread(cameraThreadHandler, 0);
+    gennThread.join();
+    cameraThread.join();
 
-#ifndef CPU_ONLY
-    if (which == GPU){
-        while (!done)
-        {
-            locust.runGPU(DT); // run next batch
-            //locust.getSpikeNumbersFromGPU();
-            //locust.getSpikesFromGPU();
-            pullDNCurrentSpikesFromDevice();
-
-            //	pullDNStateFromDevice();
-
-#ifdef TIMING
-            fprintf(timeros, "%f %f %f \n", neuron_tme, synapse_tme, learning_tme);
-#endif
-            locust.sum_spikes();
-            locust.outputDN_spikes(osf2, which);
-
-            fprintf(osf, "%f ", t);
-            //      locust.output_state(osf,which);
-            fprintf(osf,"\n");
-
-            // report progress
-            if (t - last_t_report >= T_REPORT_TME)
-            {
-                fprintf(stdout, "time %f \n", t);
-                last_t_report= t;
-            }
-            done= (t >= TOTAL_TME);
-        }
-    }
-#endif
-
-    if (which == CPU){
-        while (!done)
-        {
-            locust.runCPU(DT); // run next batch
-
-#ifdef TIMING
-            fprintf(timeros, "%f %f %f \n", neuron_tme, synapse_tme, learning_tme);
-#endif
-            locust.sum_spikes();
-            locust.outputDN_spikes(osf2, which);
-            //      locust.output_state(osf,which);
-
-            // report progress
-            if (t - last_t_report >= T_REPORT_TME)
-            {
-                fprintf(stdout, "time %f \n", t);
-                last_t_report= t;
-            }
-            done= (t >= TOTAL_TME);
-        }
-    }
-    timer.stopTimer();
 #ifndef CPU_ONLY
     if (which == GPU) pullDNStateFromDevice();
 #endif
@@ -221,6 +332,5 @@ int main(int argc, char *argv[])
         locust.free_device_mem();
     }
 #endif
-
     return 0;
 }
